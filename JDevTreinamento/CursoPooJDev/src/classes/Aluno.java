8ce0a6580f7cba0c5f2aa1469dc350e7ec51fde3
Lindomar Bassetti <Lindomar1802@gmail.com>
2020-08-15 10:42:25 -0300

    continuando a revisao de poo Jdev

--- JDevTreinamento/CursoPooJDev/src/classes/Aluno.java
+++ JDevTreinamento/CursoPooJDev/src/classes/Aluno.java
@@ -19,36 +19,9 @@ public class Aluno {
 	private double nota4;
 	
 	
-	/*get e set da nota*/
-	public double getNota1() {
-		return nota1;
-	}
-	public void setNota1(double nota1) {
-		this.nota1 = nota1;
-	}
-	public double getNota2() {
-		return nota2;
-	}
-	public void setNota2(double nota2) {
-		this.nota2 = nota2;
-	}
-	public double getNota3() {
-		return nota3;
-	}
-	public void setNota3(double nota3) {
-		this.nota3 = nota3;
-	}
-	public double getNota4() {
-		return nota4;
-	}
-	public void setNota4(double nota4) {
-		this.nota4 = nota4;
-	}
-	public double getMedia() {
-		return (nota1+nota2+nota3+nota4)/4;
-		
-	}
-	/*Getters and Setters*/
+	
+	
+	
 	public String getNome() {
 		return nome;
 	}
@@ -109,10 +82,51 @@ public class Aluno {
 	public void setSerieMatriculado(String serieMatriculado) {
 		this.serieMatriculado = serieMatriculado;
 	}
-	
-	
+	public double getNota1() {
+		return nota1;
+	}
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	public double getNota2() {
+		return nota2;
+	}
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+	public double getNota3() {
+		return nota3;
+	}
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+	public double getNota4() {
+		return nota4;
+	}
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
  
-
+	public double getMedia(){
+		return (this.nota1+this.nota2+this.nota3+this.nota4)/4;
+	}
+	public boolean getAlunoAprovado() {
+		double media = this.getMedia();
+		if (media >=7.0) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", matricula="
+				+ matricula + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
+				+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
+				+ serieMatriculado + ", nota1=" + nota1 + ", nota2=" + nota2 + ", nota3=" + nota3 + ", nota4=" + nota4
+				+ "]";
+	}
 	
 	
 }
--- JDevTreinamento/CursoPooJDev/src/executaveis/Principal.java
+++ JDevTreinamento/CursoPooJDev/src/executaveis/Principal.java
@@ -1,18 +1,63 @@
 package executaveis;
 
+import javax.swing.JOptionPane;
+
 import classes.Aluno;
 
 public class Principal {
 	public static void main(String[] args) {
-		Aluno aluno1 = new Aluno();
-		aluno1.setNome("Jao do osso");
-		System.out.println(aluno1.getNome());
-		aluno1.setNota1(90);
-		aluno1.setNota2(80.8);
-		aluno1.setNota3(70.9);
-		aluno1.setNota4(90.7);
-		System.out.println(aluno1.getMedia());
 		
-		
+	//solicita entrada de dados pelo teclado do usuario
+	String nome = JOptionPane.showInputDialog("Nome do Aluno");
+	int idade = Integer.parseInt(JOptionPane.showInputDialog("idade"));
+	String dataNascimento = JOptionPane.showInputDialog("data de nascimento");
+	String matricula = JOptionPane.showInputDialog("matricula");
+	String numeroCpf = JOptionPane.showInputDialog("cpf ");
+	String nomeMae = JOptionPane.showInputDialog("nome da mãe");
+	String nomePai = JOptionPane.showInputDialog("nome do pai");
+	String dataMatricula = JOptionPane.showInputDialog("data da matricula");
+	String nomeEscola = JOptionPane.showInputDialog("nome da escola");
+	String nomeCurso = JOptionPane.showInputDialog("serie matriculada/ nome do curso");
+	double nota1 = Double.parseDouble(JOptionPane.showInputDialog("nota 1"));
+	double nota2 = Double.parseDouble(JOptionPane.showInputDialog("nota 2"));
+	double nota3 = Double.parseDouble(JOptionPane.showInputDialog("nota 3"));
+	double nota4 = Double.parseDouble(JOptionPane.showInputDialog("nota 4"));
+	
+	//instancia um objeto da classe aluno
+	Aluno aluno1 = new Aluno();
+	
+	// utilizando as variaveis popula o objeto com as informações inseridas pelo usuario
+	
+	aluno1.setNome(nome);
+	aluno1.setIdade(idade);
+	aluno1.setDataNascimento(dataNascimento);
+	aluno1.setMatricula(matricula);
+	aluno1.setNumeroCpf(numeroCpf);
+	aluno1.setNomeMae(nomeMae);
+	aluno1.setNomePai(nomePai);
+	aluno1.setDataMatricula(dataMatricula);
+	aluno1.setNomeEscola(nomeEscola);
+	aluno1.setSerieMatriculado(nomeCurso);
+	aluno1.setNota1(nota1);
+	aluno1.setNota2(nota2);
+	aluno1.setNota3(nota3);
+	aluno1.setNota4(nota4);
+	
+	
+	
+	
+
+	
+	
+	System.out.println("media "+aluno1.getMedia());
+	System.out.println(aluno1);
+	
+	
+	
+	
+	
+	
+	System.out.println("media "+aluno1.getMedia());
+	System.out.println("aprovado: "+(aluno1.getAlunoAprovado()?"aprovado":"reprovado"));
 	}
 }
